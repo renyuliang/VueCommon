da3c7f7a3833e4e9689c51d13de8a1b6759ed524
renyuliang <785788909@qq.com>
2019-07-29 11:02:33 +0800

    调整

--- src/pages/Index/index.vue
+++ src/pages/Index/index.vue
@@ -88,12 +88,12 @@
       </el-collapse>
       <!--多选-->
       <!--<chose-Man-->
-        <!--:dialog-visible="dialogChoseMan"-->
-        <!--@on-cancel="dialogChoseMan = false"-->
-        <!--@on-confirm="getSaveList"-->
-        <!--:canChecked="true"-->
-        <!--:checkedList="roleUser.userList"-->
-        <!--:disableList="roleUser.unChooseUserIdList"/>-->
+      <!--:dialog-visible="dialogChoseMan"-->
+      <!--@on-cancel="dialogChoseMan = false"-->
+      <!--@on-confirm="getSaveList"-->
+      <!--:canChecked="true"-->
+      <!--:checkedList="roleUser.userList"-->
+      <!--:disableList="roleUser.unChooseUserIdList"/>-->
       <!--单选-->
       <chose-Man
         :dialog-visible="dialogChoseMan"
@@ -278,12 +278,12 @@
             @listenVal="listenVlaue" > // 返回值
           </>
 
-            // 变量
-            inputName: '',
+          // 变量
+          inputName: '',
 
-            // 方法
+          // 方法
           listenVlaue (val) {
-            this.inputName = val.value
+          this.inputName = val.value
           },
           </pre>
         </el-collapse-item>
@@ -323,7 +323,7 @@
 
           // 方法
           handSuccess (val) {
-            console.log(val[0].name)
+          console.log(val[0].name)
           },
           </pre>
         </el-collapse-item>
@@ -374,14 +374,14 @@
 
           // 方法
           clickImg (e) {
-            this.showImg = true
-            this.imgSrc = e.currentTarget.src
+          this.showImg = true
+          this.imgSrc = e.currentTarget.src
           }
           </pre>
         </el-collapse-item>
       </el-collapse>
       <hr>
-      <h2 id="page">公用分页,保存查询条件</h2>
+      <h2 id="page">公用,保存查询条件</h2>
       <div>
         <el-form :inline="true" :model="searchData" class="demo-form-inline" size="mini">
           <el-form-item label="产品">
@@ -735,326 +735,265 @@
   </div>
 </template>
 <script>
-import autoInput from '../../components/autoInput'
-import choseArea from '../../components/choseArea'
-import choseMore from '../../components/choseMore'
-import choseOne from '../../components/choseOne'
-import choseMan from '../../components/choseMan'
-import upload from '../../components/upload'
-import bigImg from '../../components/bigImg'
-import { isNull, isNumber, isSpecial, isChinese, isPhone, isMoney, isIdCard, changeDate, changeTimestamp } from '../../mixin/regular.js'
-// 分页公用 必须引入
-import initPageJs from '../../mixin/init-page'
-export default {
-  name: 'index',
-  components: { choseMan, choseOne, choseMore, autoInput, upload, choseArea, bigImg },
-  mixins: [initPageJs],
-  data () {
-    return {
-      // 选择医院
-      dialogChoseMore: false,
-      dialogChoseOne: false,
-      dialogUpload: false,
-      dialogChoseMan: false,
-      roleUser: {
-        unChooseUserIdList: [28],
-        userList: [
-          {
-            "authId": null,
-            "createTime": null,
-            "creatorId": null,
-            "departmentName": "成都一蟹科技有限公司",
-            "email": "wj020103@126.com",
-            "headImg": "http://file.medcrab.com/media/5befd91ddae4550b4cb8656f",
-            "id": 2,
-            "lastmodifyId": null,
-            "lastmodifyTime": null,
-            "mobile": "18080168320",
-            "name": "王军",
-            "nickName": null,
-            "orgId": null,
-            "remarks": null,
-            "sex": 1
-          }
-        ]
-      },
-      manList: [], // 选择成员
-      hospitalList: [], // 确定后保存的数据
-      // 单选
-      getName: '',
-      // 自定义搜索条件
-      searchInitList: [
-        {
-          searchStyle: 'input', // 判断搜索类型为 input
-          title: '标题11',
-          maxlength: 10,
-          searchName: 'title1', // 查询条件名称 -- 接口字段名称
-          modelName: '', // v-model的值
-          placeholder: '请输入'
-        },
-        {
-          searchStyle: 'select', // 判断搜索类型为 select 下拉
-          title: '标题22',
-          searchName: 'title2', // 查询条件名称 -- 接口字段名称
-          modelName: '', // v-model的值
-          placeholder: '请选择',
-          options: [
+  import autoInput from '../../components/autoInput'
+  import choseArea from '../../components/choseArea'
+  import choseMore from '../../components/choseMore'
+  import choseOne from '../../components/choseOne'
+  import choseMan from '../../components/choseMan'
+  import upload from '../../components/upload'
+  import bigImg from '../../components/bigImg'
+  import { isNull, isNumber, isSpecial, isChinese, isPhone, isMoney, isIdCard, changeDate, changeTimestamp } from '../../mixin/regular.js'
+  // 分页公用 必须引入
+  import initPageJs from '../../mixin/init-page'
+  export default {
+    name: 'index',
+    components: { choseMan, choseOne, choseMore, autoInput, upload, choseArea, bigImg },
+    mixins: [initPageJs],
+    data () {
+      return {
+        // 选择医院
+        dialogChoseMore: false,
+        dialogChoseOne: false,
+        dialogUpload: false,
+        dialogChoseMan: false,
+        roleUser: {
+          unChooseUserIdList: [28],
+          userList: [
             {
-              label: '张三',
-              value: '张三'
-            },
-            {
-              label: '李四',
-              value: '李四'
+              'authId': null,
+              'createTime': null,
+              'creatorId': null,
+              'departmentName': '成都一蟹科技有限公司',
+              'email': 'wj020103@126.com',
+              'headImg': 'http://file.medcrab.com/media/5befd91ddae4550b4cb8656f',
+              'id': 2,
+              'lastmodifyId': null,
+              'lastmodifyTime': null,
+              'mobile': '18080168320',
+              'name': '王军',
+              'nickName': null,
+              'orgId': null,
+              'remarks': null,
+              'sex': 1
             }
           ]
-        }
-      ],
-      // 单选 table 设置项
-      tableInitProps: [
-        {
-          name: 'name',
-          label: '姓名'
         },
-        {
-          name: 'mobile',
-          label: '电话'
+        manList: [], // 选择成员
+        hospitalList: [], // 确定后保存的数据
+        // 单选
+        getName: '',
+        // 自定义搜索条件
+        searchInitList: [
+          {
+            searchStyle: 'input', // 判断搜索类型为 input
+            title: '标题11',
+            maxlength: 10,
+            searchName: 'title1', // 查询条件名称 -- 接口字段名称
+            modelName: '', // v-model的值
+            placeholder: '请输入'
+          },
+          {
+            searchStyle: 'select', // 判断搜索类型为 select 下拉
+            title: '标题22',
+            searchName: 'title2', // 查询条件名称 -- 接口字段名称
+            modelName: '', // v-model的值
+            placeholder: '请选择',
+            options: [
+              {
+                label: '张三',
+                value: '张三'
+              },
+              {
+                label: '李四',
+                value: '李四'
+              }
+            ]
+          }
+        ],
+        // 单选 table 设置项
+        tableInitProps: [
+          {
+            name: 'name',
+            label: '姓名'
+          },
+          {
+            name: 'mobile',
+            label: '电话'
+          }
+        ],
+        // 动态输入模糊匹配
+        inputName: '',
+        // 上传成功后的文件
+        uploadName: '',
+        // 选择地区
+        areaName: '',
+        // 图片全屏查看
+        showImg: false,
+        imgSrc: '',
+        // 保存搜索条件
+        tableData22: [],
+        currentPage: 1, // 当前页码
+        pageSize: 1, // 每页的数据条数
+        searchBuffer22: {
+          name: '',
+          phone: '',
+          currentPage: 1,
+          pageSize: 1
+        },
+        // 公用分页,保存查询条件-- 查询参数名必须 searchData
+        searchData: {
+          product: '',
+          hospital: '',
+          userName: ''
+        },
+        // js
+        js: {
+          feikong: '', // 非空验证
+          number: '', // 整数验证
+          special: '', // 特殊字符
+          chinese: '', // 汉字验证
+          phone: '', // 电话验证
+          money: '', // 金额验证
+          card: '', // 身份证验证
+          selectValue: '+', // 判断计算类型
+          firstNum: '',
+          lastNum: ''
+        },
+        // 返回结果
+        result: {
+          feikong: '',
+          number: '',
+          special: '',
+          chinese: '',
+          phone: '',
+          money: '',
+          card: '',
+          jingduBeforeSum: '', // 精度调试之前的结果
+          jingduAfterSum: ''// 精度调试之后的结果
         }
-      ],
-      // 动态输入模糊匹配
-      inputName: '',
-      // 上传成功后的文件
-      uploadName: '',
-      // 选择地区
-      areaName: '',
-      // 图片全屏查看
-      showImg: false,
-      imgSrc: '',
-      // 保存搜索条件
-      tableData22: [],
-      currentPage: 1, // 当前页码
-      pageSize: 1, // 每页的数据条数
-      searchBuffer22: {
-        name: '',
-        phone: '',
-        currentPage: 1,
-        pageSize: 1
+      }
+    },
+    methods: {
+      // 选择人员
+      getSaveList (val) {
+        this.dialogChoseMan = false
+        this.manList = val
+        console.log(val)
       },
-      // 分页公用 -- 查询参数名必须 searchData
-      searchData: {
-        product: '',
-        hospital: '',
-        userName: ''
+      // 确定 -- 多选
+      confirmHospitalList (val) {
+        this.dialogChoseMore = false
+        this.hospitalList = val
+        console.log(this.hospitalList)
+      },
+      // 确定 -- 单选
+      confirmName (val) {
+        this.dialogChoseOne = false
+        this.getName = val.name
+      },
+      // 输入框模糊匹配
+      listenVlaue (val) {
+        this.inputName = val.value
+      },
+      // 上传成功后的操作
+      handSuccess (val) {
+        this.uploadName = val[0].name
+      },
+      // 获取地区
+      getAreaName (val) {
+        this.areaName = val
+      },
+      clickImg (e) {
+        this.showImg = true
+        this.imgSrc = e.currentTarget.src
+      },
+      // 分页公用 -- 列表方法名必须是getListData
+      getListData () {
+        let _this = this
+        _this.searchBuffer.pageNum = _this.pageNum
+        _this.searchBuffer.pageSize = _this.pageSize
+        _this.$axios.get('../../static/queryPage.json', _this.searchBuffer).then((res) => {
+          _this.tableData = res.data.list
+          _this.total = res.data.total
+        })
       },
       // js
-      js: {
-        feikong: '', // 非空验证
-        number: '', // 整数验证
-        special: '', // 特殊字符
-        chinese: '', // 汉字验证
-        phone: '', // 电话验证
-        money: '', // 金额验证
-        card: '', // 身份证验证
-        selectValue: '+', // 判断计算类型
-        firstNum: '',
-        lastNum: ''
-      },
-      // 返回结果
-      result: {
-        feikong: '',
-        number: '',
-        special: '',
-        chinese: '',
-        phone: '',
-        money: '',
-        card: '',
-        jingduBeforeSum: '', // 精度调试之前的结果
-        jingduAfterSum: ''// 精度调试之后的结果
-      }
-    }
-  },
-  mounted () {
-    this.loadTableData()
-  },
-  methods: {
-    // 选择人员
-    getSaveList (val) {
-      this.dialogChoseMan = false
-      this.manList = val
-      console.log(val)
-    },
-    // 确定 -- 多选
-    confirmHospitalList (val) {
-      this.dialogChoseMore = false
-      this.hospitalList = val
-      console.log(this.hospitalList)
-    },
-    // 确定 -- 单选
-    confirmName (val) {
-      this.dialogChoseOne = false
-      this.getName = val.name
-    },
-    // 输入框模糊匹配
-    listenVlaue (val) {
-      this.inputName = val.value
-    },
-    // 上传成功后的操作
-    handSuccess (val) {
-      this.uploadName = val[0].name
-    },
-    // 获取地区
-    getAreaName (val) {
-      this.areaName = val
-    },
-    clickImg (e) {
-      this.showImg = true
-      this.imgSrc = e.currentTarget.src
-    },
-    // 存储搜索条件
-    loadTableData () {
-      let vm = this
-      if (sessionStorage.length > 0) {
-        vm.searchBuffer22 = {}
-        vm.searchBuffer22 = Object.assign(vm.searchBuffer22, sessionStorage)
-      }
-      vm.tableData22 = [
-        {
-          date: "2016-05-02",
-          name: "王小虎1",
-          phone: "13219941001"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎2",
-          phone: "13219941002"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎3",
-          phone: "13219941003"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎4",
-          phone: "13219941004"
+      // 非空验证
+      blurFeikong () {
+        if (!isNull(this.js.feikong)) {
+          this.result.feikong = '正确'
+        } else {
+          this.result.feikong = '错误'
         }
-      ]
-    },
-    handleSizeChange (val) {
-      console.log(`每页 ${val} 条`)
-      this.currentPage = 1
-      this.pageSize = val
-      this.searchBuffer22.pageSize = val
-    },
-    handleCurrentChange (val) {
-      console.log(`当前页: ${val}`)
-      this.currentPage = val
-      // this.storeNum = val
-      this.searchBuffer22.currentPage = val
-    },
-    storeSearch () {
-      let vm = this
-      for (var i in vm.searchBuffer22) {
-        sessionStorage.setItem(i, vm.searchBuffer22[i])
-      }
-      console.log(sessionStorage)
-    },
-    gobaidu () {
-      this.storeSearch()
-      window.location.reload()
-    },
-    reset () {
-      sessionStorage.clear()
-      this.searchBuffer22.name = ''
-      this.searchBuffer22.phone = ''
-    },
-    // 分页公用 -- 列表方法名必须是getListData
-    getListData () {
-      let _this = this
-      _this.searchBuffer.pageNum = _this.pageNum
-      _this.searchBuffer.pageSize = _this.pageSize
-      _this.$axios.get('../../static/queryPage.json', _this.searchBuffer).then((res) => {
-        _this.tableData = res.data.list
-        _this.total = res.data.total
-      })
-    },
-    // js
-    // 非空验证
-    blurFeikong () {
-      if (!isNull(this.js.feikong)) {
-        this.result.feikong = '正确'
-      } else {
-        this.result.feikong = '错误'
-      }
-    },
-    // 整数验证
-    blurNumber () {
-      if (isNumber(this.js.number)) {
-        this.result.number = '正确'
-      } else {
-        this.result.number = '错误'
-      }
-    },
-    // 特殊字符验证
-    blurSpecial () {
-      if (!isSpecial(this.js.special)) {
-        this.result.special = '正确'
-      } else {
-        this.result.special = '错误'
-      }
-    },
-    // 汉字验证
-    blurChinese () {
-      if (!isChinese(this.js.chinese)) {
-        this.result.chinese = '正确'
-      } else {
-        this.result.chinese = '错误'
-      }
-    },
-    // 手机号验证
-    blurPhone () {
-      if (!isPhone(this.js.phone)) {
-        this.result.phone = '正确'
-      } else {
-        this.result.phone = '错误'
-      }
-    },
-    // 金额验证
-    blurMoney () {
-      if (!isMoney(this.js.money)) {
-        this.result.money = '正确'
-      } else {
-        this.result.money = '错误'
-      }
-    },
-    // 身份证验证
-    blurCard () {
-      if (!isIdCard(this.js.card)) {
-        this.result.card = '正确'
-      } else {
-        this.result.card = '错误'
-      }
-    },
-    getSave () {
-      if (this.js.selectValue === '+') {
-        this.result.jingduBeforeSum = Number(this.js.firstNum) + Number(this.js.lastNum)
-        this.result.jingduAfterSum = this.$floatObj.add(this.js.firstNum, this.js.lastNum)
-      }
-      if (this.js.selectValue === '-') {
-        this.result.jingduBeforeSum = Number(this.js.firstNum) - Number(this.js.lastNum)
-        this.result.jingduAfterSum = this.$floatObj.subtract(this.js.firstNum, this.js.lastNum)
-      }
-      if (this.js.selectValue === '*') {
-        this.result.jingduBeforeSum = Number(this.js.firstNum) * Number(this.js.lastNum)
-        this.result.jingduAfterSum = this.$floatObj.multiply(this.js.firstNum, this.js.lastNum)
-      }
-      if (this.js.selectValue === '/') {
-        this.result.jingduBeforeSum = Number(this.js.firstNum) / Number(this.js.lastNum)
-        this.result.jingduAfterSum = this.$floatObj.divide(this.js.firstNum, this.js.lastNum, 2)
+      },
+      // 整数验证
+      blurNumber () {
+        if (isNumber(this.js.number)) {
+          this.result.number = '正确'
+        } else {
+          this.result.number = '错误'
+        }
+      },
+      // 特殊字符验证
+      blurSpecial () {
+        if (!isSpecial(this.js.special)) {
+          this.result.special = '正确'
+        } else {
+          this.result.special = '错误'
+        }
+      },
+      // 汉字验证
+      blurChinese () {
+        if (!isChinese(this.js.chinese)) {
+          this.result.chinese = '正确'
+        } else {
+          this.result.chinese = '错误'
+        }
+      },
+      // 手机号验证
+      blurPhone () {
+        if (!isPhone(this.js.phone)) {
+          this.result.phone = '正确'
+        } else {
+          this.result.phone = '错误'
+        }
+      },
+      // 金额验证
+      blurMoney () {
+        if (!isMoney(this.js.money)) {
+          this.result.money = '正确'
+        } else {
+          this.result.money = '错误'
+        }
+      },
+      // 身份证验证
+      blurCard () {
+        if (!isIdCard(this.js.card)) {
+          this.result.card = '正确'
+        } else {
+          this.result.card = '错误'
+        }
+      },
+      getSave () {
+        if (this.js.selectValue === '+') {
+          this.result.jingduBeforeSum = Number(this.js.firstNum) + Number(this.js.lastNum)
+          this.result.jingduAfterSum = this.$floatObj.add(this.js.firstNum, this.js.lastNum)
+        }
+        if (this.js.selectValue === '-') {
+          this.result.jingduBeforeSum = Number(this.js.firstNum) - Number(this.js.lastNum)
+          this.result.jingduAfterSum = this.$floatObj.subtract(this.js.firstNum, this.js.lastNum)
+        }
+        if (this.js.selectValue === '*') {
+          this.result.jingduBeforeSum = Number(this.js.firstNum) * Number(this.js.lastNum)
+          this.result.jingduAfterSum = this.$floatObj.multiply(this.js.firstNum, this.js.lastNum)
+        }
+        if (this.js.selectValue === '/') {
+          this.result.jingduBeforeSum = Number(this.js.firstNum) / Number(this.js.lastNum)
+          this.result.jingduAfterSum = this.$floatObj.divide(this.js.firstNum, this.js.lastNum, 2)
+        }
       }
     }
   }
-}
 </script>
 
 <style lang="scss">
