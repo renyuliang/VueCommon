74567228ab4cc19c08b17cc54dae9f91045367e8
renyuliang <785788909@qq.com>
2020-01-02 17:10:01 +0800

    优化

--- src/pages/compents/commonDate.vue
+++ src/pages/compents/commonDate.vue
@@ -6,6 +6,7 @@
       <a href="#date3">(年 月)日期范围 2个选择框</a>
     </div>
     <br>
+    <h2>日期选择</h2>
     <div id="date1">
       <el-date-picker
         v-model="date1"
@@ -42,6 +43,7 @@
       </el-collapse>
     </div>
     <br>
+    <h2>日期选择（范围）</h2>
     <div id="date2">
       <el-date-picker
         v-model="dateStart"
@@ -132,9 +134,19 @@
       </el-collapse>
     </div>
     <br>
+    <h2>年月选择（范围）</h2>
     <el-date-picker v-model.trim="monthStart" :picker-options="optionMonthStart" @change="selectMonthStart" :clearable="false" :editable="false" type="month" value-format="yyyy-MM" placeholder="月份" />
     -
     <el-date-picker v-model.trim="monthEnd" :picker-options="optionMonthEnd" @change="selectMonthEnd" :clearable="false" :editable="false" type="month" value-format="yyyy-MM" placeholder="月份" />
+    <br>
+    <h2>计算2个日期之间的时间差</h2>
+    第一个日期：2019-11-01
+    <br>
+    第二个日期：2020-01-02
+    <br>
+    <el-button type="primary" @click="monthDayDiff">计算</el-button>
+    <br>
+    {{getDateResult}}
   </div>
 </template>
 
@@ -143,6 +155,7 @@
     name: 'commonDate',
     data () {
       return {
+        getDateResult: '',
         date1: '',
         Options1: {
           disabledDate(time) {
@@ -238,6 +251,35 @@
             }
           }
         }
+      },
+      monthDayDiff() {
+        let _this = this
+        let flag = [1, 3, 5, 7, 8, 10, 12, 4, 6, 9, 11, 2]
+        let start = new Date('2019-11-01')
+        let end = new Date('2020-01-02')
+        let year = end.getFullYear() - start.getFullYear();
+        let month = end.getMonth() - start.getMonth()
+        let day = end.getDate() - start.getDate()
+        if (month < 0) {
+          year--;
+          month = end.getMonth() + (12 - start.getMonth())
+        }
+        if (day < 0) {
+          month--
+          let index = flag.findIndex((temp) => {
+            return temp === start.getMonth() + 1
+          })
+          let monthLength
+          if (index <= 6) {
+            monthLength = 31
+          } else if (index > 6 && index <= 10) {
+            monthLength = 30
+          } else {
+            monthLength = 28
+          }
+          day = end.getDate() + (monthLength - start.getDate())
+        }
+        _this.getDateResult = `相差${year}年${month}月${day}天`
       }
     }
   }
